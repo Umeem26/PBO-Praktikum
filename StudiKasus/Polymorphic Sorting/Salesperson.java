beba581fa9c71ad6173d974082791ee8975efe94
Umeem26 <hisyamk.u5062@gmail.com>
2025-10-02 15:08:58 +0700

    Add Studi Kasus 2: Polymorphic Sorting

---
+++ StudiKasus/Polymorphic Sorting/Salesperson.java
@@ -0,0 +1,48 @@
+public class Salesperson implements Comparable
+{
+    private String firstName, lastName;
+    private int totalSales;
+
+    public Salesperson (String first, String last, int sales)
+    {
+        firstName = first;
+        lastName = last;
+        totalSales = sales;
+    }
+
+    public String toString()
+    {
+        return lastName + ", " + firstName + ": \t" + totalSales;
+    }
+
+    public boolean equals (Object other)
+    {
+        return (lastName.equals(((Salesperson)other).getLastName()) &&
+        firstName.equals(((Salesperson)other).getFirstName()));
+    }
+
+    // PERUBAHAN: Metode compareTo sekarang sudah diimplementasikan
+    public int compareTo(Object other)
+    {
+        Salesperson otherSalesperson = (Salesperson) other;
+
+        // Pertama, bandingkan total penjualan
+        if (this.totalSales != otherSalesperson.totalSales) {
+            // Urutan natural (menaik), akan dibalik oleh insertionSort
+            return this.totalSales - otherSalesperson.totalSales;
+        } else {
+            // Jika penjualan sama, bandingkan nama belakang secara terbalik (Z-A)
+            int lastNameCompare = otherSalesperson.lastName.compareTo(this.lastName);
+            if (lastNameCompare != 0) {
+                return lastNameCompare;
+            } else {
+                // Jika nama belakang sama, bandingkan nama depan secara terbalik (Z-A)
+                return otherSalesperson.firstName.compareTo(this.firstName);
+            }
+        }
+    }
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getSales() { return totalSales; }
+}
